5ac7ed3da414d39970f65a7d9d5277a1df5ca763
Harvey-46904 <harveympv@hotmail.com>
2022-09-12 13:59:52 -0500

    nuevo canvas

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,25 +1,15 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
 import logo from "../assets/img/construccion.png"
 import '../styles/App.css'
 class Inicio extends React.Component {
 
     render() { 
-        return ( <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          HOLA MUNDO STEVEN
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sigueme :)
-          </a>
-        </header>
-      </div>  );
+        return ( 
+          <Canvas>
+            
+          </Canvas>
+         );
     }
 }
  
